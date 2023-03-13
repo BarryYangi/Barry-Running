acd29024fe5e38209d187cec07245bd132fb50fa
BarryYangi <2059484047@qq.com>
2023-03-14 01:26:07 +0800

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     siteTitle: 'Barry Running',
     siteUrl: 'https://running.yct.ee',
     logo: 'https://blog.yct.ee/medias/favicon.webp',
-    description: 'Just Personal site and blog',
+    description: 'Personal site and blog',
     navLinks: [
       {
         name: 'Blog',
